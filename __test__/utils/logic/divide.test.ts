61cf8873c1ed4e95bafe1b496335cbb6f9f3dc1b
Donato Monzon <spartanhalokitty@gmail.com>
2023-08-18 10:27:30 -0600

    feat: :sparkles: save solution

--- __test__/utils/logic/divide.test.ts
+++ __test__/utils/logic/divide.test.ts
@@ -9,39 +9,52 @@ describe('tests on divide function', () => {
     expect(typeof result).toBe('number')
   })
 
-  test('divide(1) should return Infinity if only we passed one argument', () => {
-
+  test('divide(1) should return 0 if only we passed one argument', () => {
+    const result = divide(1)
+    
+    // We assumed that second is 0. Every number divided by 0 should be Infinity
+    expect(result).toBe(Infinity)
   })
 
   test('divide(100,1) should be 100', () => {
+    const result = divide(100,1)
 
+    // any number divided by 1 must be the same number
+    expect(result).toBe(100)
   })
 
   test('divide(9,3) should return 3', () => {
-
+    const result = divide(9,3)
+    expect(result).toBe(3)
   })
   
   test('divide(25,5) should return 5', () => {
-
+    const result = divide(25,5)
+    expect(result).toBe(5)
   })
 
   test('divide(81,9) should be 9', () => {
-
+    const result = divide(81,9)
+    expect(result).toBe(9)
   })
 
   test('divide(500,25) should 20', () => {
-
+    const result = divide(500,25)
+    expect(result).toBe(20)
   })
 
   test('divide(2,4) should be 0.5 (float)', () => {
-
+    const result = divide(2,4)
+    expect(result).toBe(0.5)
   })
 
   test('divide(100, 3) should be 0.33 (float)', () => {
-
+    const result = divide(100,3, { decimalsLimit: 2 })
+    expect(result).toBe(33.33)
   })
 
   test('divide(9, 16) should be 0.5625 (float)', () => {
-
+    const result = divide(9, 16, { decimalsLimit: 4 })
+    expect(result).toBe(0.5625)
   })
 })
--- src/utils/logic/divide.ts
+++ src/utils/logic/divide.ts
@@ -13,7 +13,65 @@
  * 👀 if the result is a decimal number (float), it should to contain a precision of at least 2 decimals: (100 / 3 => 3.33)
  */
 
-export const divide = (a: number = 0, b: number = 0 ): number => {
+type Divide = (a?: number, b?: number, config?: { decimalsLimit: number }) => number
 
-  return 0
+// dividing without / operator
+export const divide: Divide = (a: number = 0, b: number = 0, config ) => {
+  let int = 0
+  let float = 0
+  let decimals = []
+  let hasZeroPoint = false
+  let firstIteration = true
+
+  if( a === 0 && b === 0) return 0
+  if( a !== 0 && b === 0) return Infinity
+
+  const limit = config?.decimalsLimit || 10
+  
+  while( a > 0 && decimals.length <= limit ) {
+    if( decimals.length === 0 && a >= b ) {
+      // if there is decimal and "a" is greater than "b"
+      a = a - b
+      int++
+    } else {
+      // if there is not decimals and "a" is greater than "b"
+
+      if( a >= b ) {
+        // if "a" is greater or equal than "b"
+        a = a - b
+        float++
+      } else {
+        // if "a" is less or equal than to "b"
+        a = a * 10
+
+        decimals.push(float)
+        float = 0
+
+        if( firstIteration ) {
+          hasZeroPoint = true
+        }
+      }
+    }
+    firstIteration = false
+  }
+ 
+  if( decimals.length > 0 ) {
+    // add last count to decimals
+    decimals.push(float)
+    
+    // remove first zero on decimals
+    decimals.shift()
+ 
+    if( hasZeroPoint ) {
+      const result = Number( '0.' + decimals.join('') )
+      return result
+    }
+    else {
+      const result = Number( int + '.' + decimals.join('') )
+      return result
+    }
+  }
+ 
+  // add last count to integers
+  return int + float
 }
